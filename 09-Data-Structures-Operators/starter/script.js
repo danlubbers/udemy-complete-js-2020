e5181806a39a9fe9b0ea630ea9fe9c28f3a2276e
Dan Lubbers <lubbers.dan@gmail.com>
2021-02-16 21:28:38 -0500

    Data Structures: Finished Code Challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,29 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    // console.log(
+    //   `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delievered to ${address} at ${time}`
+    // );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here are your pasta ingredients: ${ing1}, ${ing2}, ${ing3}!`);
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    // console.log(mainIngredient, otherIngredients);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +50,227 @@ const restaurant = {
     },
   },
 };
+
+const arr = [1, 2, 3];
+
+/** ARRAY DESTRUCTURING */
+const [a, b, c] = arr;
+// console.log(a, b, c);
+
+let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+/** Switching variables */
+// The way before destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+// With destructuring
+[main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// console.log(restaurant.order(2, 0));
+// Recieve 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// Default Values - r has a default value of 1
+const [p, q, r = 1] = [8, 9];
+// console.log(p, q, r);
+
+/** OBJECT DESTRUCTURING */
+const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// reassign key names
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// Mutating variables
+let o = 111;
+let s = 999;
+const obj = { o: 23, s: 7, c: 14 };
+
+({ o, s } = obj);
+// console.log(o, s);
+
+// Nested Objects
+const {
+  fri: { open, close },
+} = openingHours;
+// console.log(open, close);
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+/** REST */
+const arrRest = [7, 8, 9];
+
+const arrWithRest = [1, 2, ...arrRest];
+// console.log(arrWithRest); // logs array
+// console.log(...arrWithRest); // logs all individual elements from array
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// Copy Array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 or more arrays
+const joinArray = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// Iterables: arrays, strings, maps, sets. Now Objects
+const str = `Dan`;
+// console.log([...str, ' ', 's']);
+
+// Real world example
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt("Let's make pasta! Ingredient 2?"),
+//   prompt("Let's make pasta! Ingredient 3?"),
+// ];
+
+// restaurant.orderPasta(...ingredients);
+
+// Objects rest Operator
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+// console.log(newRestaurant);
+
+// Copying Object without mutating original
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+
+/** Destructuring SPREAD OPERATOR */
+
+// spread because RIGHT side of assignment operator =
+// Values separated by comma
+const arrSpread = [1, 2, ...[3, 4]];
+
+// rest, because on LEFT side of assignment operator =
+// Variable names separated by comma, not values
+const [ab, bc, ...rest] = [1, 2, 3, 4, 5];
+
+const [pizza, , risotto, ...restFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+// console.log(pizza, risotto, restFood);
+
+// Destructuring Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+// Destructuring Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  // console.log(sum);
+  return sum;
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onions', 'spinach');
+
+/*** CODE CHALLENGE 1 ***/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Create player array for each team
+const [players1, players2] = game.players;
+// console.log(players1, players2);
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5.
+const { team1, x: draw, team2 } = game.odds;
+console.log(draw);
+
+// 6.
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored!`);
+};
+
+printGoals('Davies', 'Mullers', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win!');
+team1 > team2 && console.log('Team 2 is more likely to win!');
