25578d6d79dd0c851f271413d38b0e373d59cc07
Dan Lubbers <lubbers.dan@gmail.com>
2021-03-03 22:08:04 -0500

    Advanced DOM/Events: Finished Lessons 195-198

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -162,6 +162,7 @@ const initialCoords = section1.getBoundingClientRect();
 // });
 
 /*** STICKY NAVIGATION: INTERSECTION OBSERVER API ***/
+
 // const observerCB = (entries, observer) => {
 //   entries.forEach(entry => console.log(entry));
 // };
@@ -178,7 +179,7 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = entries => {
   const [entry] = entries; // destructuring to get first entry out of entries
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) {
     nav.classList.add('sticky');
   } else {
@@ -193,14 +194,150 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+/*** REVEALING ELEMENTS ON SCROLL ***/
+const allSections = document.querySelectorAll('.section'); // returns a node list of all elements
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target); // performance enhancement: Don't need to keep observing once sections have already been observed
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+/*** LAZY LOADING IMAGES ***/
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImg = (entries, observer) => {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  // If we don't use load event, blur filter gets removed immediately and slower connections will see the pixelated image
+  entry.target.addEventListener('load', () => {
+    // Remove class with blur only after image is loaded now
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px', // load 200px before we reach each image
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+/*** SLIDER FUNCTIONALITY ***/
+
+const slider = () => {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // FUNCTIONS
+  const createDots = () => {
+    slides.forEach((_, idx) => {
+      // create html element as the last child
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${idx}"></button>`
+      );
+    });
+  };
+
+  const activateDot = slide => {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide='${slide}']`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = curSlide => {
+    slides.forEach(
+      (slide, idx) =>
+        (slide.style.transform = `translateX(${100 * (idx - curSlide)}%)`)
+      // curSlide = 1: -100%, 0%, 100%, 200% ...
+    );
+  };
+
+  // Next Slide
+  const nextSlide = () => {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  // Prev Slide
+  const prevSlide = () => {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = () => {
+    goToSlide(0); // starts at slide 0
+    createDots();
+    activateDot(0);
+  };
+
+  init();
+
+  // Event Handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  // Slide using Keyboard
+  document.addEventListener('keydown', e => {
+    e.key === 'ArrowLeft' && prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 /** SELECTING, CREATING, DELETING ELEMENTS **/
 // console.log(document.documentElement); // selection of the entire html page
 // console.log(document.head); // selection of the head
 // console.log(document.body); // selection of the body
-
-const allSections = document.querySelectorAll('.section'); // returns a node list of all elements
 
 // console.log(allSections);
 
