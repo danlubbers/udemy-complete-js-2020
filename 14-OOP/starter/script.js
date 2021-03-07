e7ef6bbff9f629cdb59d6f3ca304a31758c614dc
Dan Lubbers <lubbers.dan@gmail.com>
2021-03-06 19:40:15 -0500

    OOP: Finished Code Challenge 4 and this section of the course

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -249,7 +249,7 @@ const InheritPerson = function (firstName, birthYear) {
 };
 
 InheritPerson.prototype.calcAge = function () {
-  console.log(2021 - this.birthYear);
+  // console.log(2021 - this.birthYear);
 };
 
 const Student = function (firstName, birthYear, course) {
@@ -262,18 +262,18 @@ const Student = function (firstName, birthYear, course) {
 Student.prototype = Object.create(InheritPerson.prototype);
 
 Student.prototype.introduce = function () {
-  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+  // console.log(`My name is ${this.firstName} and I study ${this.course}`);
 };
 
 const mike = new Student('Mike', 2000, 'Computer Science');
 mike.introduce();
 mike.calcAge(); //  this only works due to Object.create
 
-console.log(mike.__proto__);
-console.log(mike.__proto__.__proto__);
+// console.log(mike.__proto__);
+// console.log(mike.__proto__.__proto__);
 
 Student.prototype.constructor = Student;
-console.dir(Student.prototype.constructor);
+// console.dir(Student.prototype.constructor);
 
 // Code Challenge 3
 
@@ -292,13 +292,178 @@ EV.prototype.chargeBattery = function (chargeTo) {
 EV.prototype.accelerate = function () {
   this.speed += 20;
   this.charge--;
-  console.log(
-    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
-  );
+  // console.log(
+  //   `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  // );
 };
 
 const tesla = new EV('Tesla', 120, 23);
-console.log(tesla);
+// console.log(tesla);
 tesla.chargeBattery(77);
-console.log(tesla);
+// console.log(tesla);
 tesla.accelerate();
+
+/*** ES6: INHERITANCE BETWEEN CLASSES***/
+
+// THis way links to prototypes behind the scenes
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // call super instead of PersonCl.call(this, ...)
+    // super is how we access the 'this' keyword
+    super(fullName, birthYear); // super is the constructor function of the parent class
+    this.course = course;
+  }
+
+  introduce() {
+    // console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  // this calcAge overwrites the one in PersonCl class
+  calcAge() {
+    // console.log(
+    //   `I'm ${2021 - this.birthYear} years old, but I feel more like ${
+    //     2021 - this.birthYear + 10
+    //   }`
+    // );
+  }
+}
+
+const marta = new StudentCl('Marta Jones', 2000, 'Web Development');
+// console.log(marta);
+marta.introduce();
+marta.calcAge();
+
+/*** Object.create(): INHERITANCE BETWEEN CLASSES***/
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  // console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Javascript');
+jay.introduce();
+jay.calcAge();
+
+/*** ANOTHER CLASS EXAMPLE / Encapsulation ***/
+class Account {
+  // Pubic Fields (available on instances not prototype)
+  locale = navigator.language;
+
+  // Private Fields # makes field private (available on instances not prototype)
+  #movements = [];
+  #pin; // similar to creating empty variable
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected Property with _
+    this.#pin = pin; // redefine pin with user input
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    // console.log(`Thanks for opening an account ${this.owner}`);
+  }
+
+  // Pubic Methods:
+  // Public interface "API"
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(value) {
+    this.#movements.push(value);
+    return this; // this return helps to make the methods chainable
+  }
+
+  withdrawal(value) {
+    this.deposit(-value);
+    return this;
+  }
+
+  requestLoan(value) {
+    if (this.#approveLoan()) {
+      this.deposit(value);
+      // console.log('Loan Approved!!');
+      return this;
+    }
+  }
+
+  // Static method
+  static helper() {
+    console.log('Helper!');
+  }
+
+  // Private Methods ( no browser currently supports this!!! )
+  #approveLoan(value) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+acc1.deposit(250);
+acc1.withdrawal(140);
+acc1.requestLoan(1000);
+// acc1._approveLoan(2000);
+
+// console.log(acc1);
+// console.log(acc1.getMovements());
+// console.log(acc1.#movements); // can not access private variable outside of class
+
+/*** CHAINING METHODS ***/
+acc1
+  .deposit(300)
+  .deposit(500)
+  .withdrawal(35)
+  .requestLoan(25000)
+  .withdrawal(4000);
+// console.log(acc1.getMovements()); // check to make sure chaining worked
+
+// Code Challenge 4
+
+class EVCl extends Car2 {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  accelerate() {
+    this.speed += 25;
+    this.#charge--;
+    return this;
+  }
+
+  chargeBattery(value) {
+    this.#charge = value;
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+
+rivian.accelerate();
+console.log(rivian);
+rivian.brake();
+console.log(rivian);
+rivian.chargeBattery(50);
+console.log(rivian);
+
+// Chaining
+rivian.accelerate().accelerate().chargeBattery(20).brake();
+console.log(rivian);
+
+console.log(rivian.speedUS); // has access to the getters and setters on parent class
