862163d8c5dd3d5baacc84ea274765dc00dbf35c
Dan Lubbers <lubbers.dan@gmail.com>
2021-03-13 19:09:50 -0500

    Async JS: Finished 258 - 262

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -56,7 +56,7 @@ const renderCountry = (data, className = '') => {
     `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  // countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 // AJAX CALL country 1
@@ -286,22 +286,161 @@ const createImage = imgPath => {
 
 let currentImg;
 
-createImage('/img/img-1.jpg')
-  .then(img => {
-    currentImg = img;
-    console.log('Image 1 loaded!');
-    return waitChallenge(2);
-  })
-  .then(() => {
-    currentImg.style.display = 'none';
-    return createImage('img/img-2.jpg');
-  })
-  .then(img => {
-    currentImg = img;
-    console.log('Image 2 loaded!');
-    return waitChallenge(2);
-  })
-  .then(() => {
-    currentImg.style.display = 'none';
-  })
+// createImage('/img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 1 loaded!');
+//     return waitChallenge(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 2 loaded!');
+//     return waitChallenge(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//   })
+//   .catch(err => console.error(err));
+
+/*** CONSUMING ASYNC/AWAIT ***/
+/*** ERROR HANDLING: TRY/CATCH ***/
+
+const getPositionAsync = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmIAsync = async function () {
+  try {
+    // Geolocation
+    const pos = await getPositionAsync();
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    // Reverse Geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!resGeo.ok) throw new Error(`Problem getting location data!`);
+    const dataGeo = await resGeo.json();
+    // console.log(dataGeo);
+
+    // Country Data
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+    );
+    if (!res.ok) throw new Error(`Problem getting Country data!`);
+    const data = await res.json();
+    renderCountry(data[0]);
+
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+  } catch (err) {
+    console.error(err);
+    renderError(`Something went wrong: ${err.message}`);
+
+    // reject promise returned from async function
+    throw err;
+  }
+};
+
+console.log('1: Will get location');
+
+// return the promise value 'You are in city, country'
+// whereAmIAsync()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err}`))
+//   .finally(() => console.log(`3: Finished getting location`));
+
+// using async/await using IIFE instead of then/catch above
+(async function () {
+  try {
+    const city = await whereAmIAsync();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err}`);
+  }
+  console.log(`3: Finished getting location`);
+})();
+
+/*** PROMISES IN PARALLEL ***/
+
+const getJSON = (url, errorMsg = 'Something went wrong!') => {
+  return fetch(url).then(res => {
+    if (!res.ok) throw new Error(`${errorMsg} (${res.status})`);
+    return res.json();
+  });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+
+    // Promise.all - If one promise rejects, they all fail
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+
+    console.log(data.map(el => el[0].capital)); // loop over data to get capital
+
+    // console.log([data1.capital, data2.capital, data3.capital]);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
+
+/*** PROMISE COMBINATORS ***/
+
+// Promise.race
+// only get one result of the fastest promise, either fulfilled or rejected
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.eu/rest/v2/name/italy`),
+    getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
+    getJSON(`https://restcountries.eu/rest/v2/name/mexico`),
+  ]);
+
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error('request took to long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
+  timeout(0.3), // rejects at 0.1
+])
+  .then(res => console.log(res))
   .catch(err => console.error(err));
+
+// Promise.allSettled *NEW ES2020*
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Success'),
+]).then(res => console.log(res));
+
+// Promise.any *NEW ES2021*
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Success'),
+]).then(res => console.log(res));
